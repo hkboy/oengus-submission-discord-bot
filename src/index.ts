dcee8cebce22937bd013e120cdfa929922bfab6f
oliver <o-lohr@gmx.de>
2019-10-22 16:39:29 +0200

    make sure to disconnect from discord, even if another error happens

--- src/index.ts
+++ src/index.ts
@@ -66,7 +66,11 @@ async function loginBot(): Promise<Discord.Client> {
             discordClient.login(DISCORD_BOT_TOKEN);
             Promise.race([
                 //30 sec timeout
-                new Promise((_, reject) => setTimeout(() => reject(new Error('timeout')), 30 * 1000)),
+                new Promise((_, reject) => setTimeout(() => {
+                    // if it's a timeout reject and destroy the client
+                    discordClient.destroy();
+                    reject(new Error('timeout'))
+                    }, 30 * 1000)),
                 new Promise((resolve, _) => discordClient.on('ready',() => resolve())),
             ]).then(() => {
                 resolve(discordClient);
@@ -112,34 +116,44 @@ async function getAlreadyAnnouncedCategoryIDs(channel: Discord.TextChannel): Pro
 }
 
 async function announceNotAlreadyPostedSubmissions(): Promise<void> {
-    const discordClient = await loginBot();
-    //const poggersEmoji = discordClient.guilds.get(DISCORD_GUILD).emojis.find(emoji => emoji.name == "Magoo");
-    const channel = discordClient.channels.get(SUBMISSION_CHANNEL) as Discord.TextChannel;
-    if (!channel) {
-        throw new Error("channel doesn't exist!");
+    let discordClient: Discord.Client;
+    try {
+        discordClient = await loginBot();
+    } catch(e) {
+        console.error("Bot couldn't login!", e);
     }
-    let [submissions, announcedCategoryIDs] = await Promise.all([getSubmissions(), getAlreadyAnnouncedCategoryIDs(channel)]);
-    let unannounced: GameCategoryCombined[] = [];
-    submissions.forEach(submission => {
-        submission.categories.forEach(category => {
-            if (!announcedCategoryIDs.includes(category.id)) {
-                unannounced.push({
-                    catDesc: category.description,
-                    catId: category.id,
-                    categoryName: category.name,
-                    consoleName: submission.console,
-                    gameDesc: submission.description,
-                    gameName: submission.name,
-                    userName: submission.user.username,
-                });
-            }
+    try {
+        //const poggersEmoji = discordClient.guilds.get(DISCORD_GUILD).emojis.find(emoji => emoji.name == "Magoo");
+        const channel = discordClient.channels.get(SUBMISSION_CHANNEL) as Discord.TextChannel;
+        if (!channel) {
+            throw new Error("channel doesn't exist!");
+        }
+        let [submissions, announcedCategoryIDs] = await Promise.all([getSubmissions(), getAlreadyAnnouncedCategoryIDs(channel)]);
+        let unannounced: GameCategoryCombined[] = [];
+        submissions.forEach(submission => {
+            submission.categories.forEach(category => {
+                if (!announcedCategoryIDs.includes(category.id)) {
+                    unannounced.push({
+                        catDesc: category.description,
+                        catId: category.id,
+                        categoryName: category.name,
+                        consoleName: submission.console,
+                        gameDesc: submission.description,
+                        gameName: submission.name,
+                        userName: submission.user.username,
+                    });
+                }
+            });
         });
-    });
-    for (let i = 0;i<unannounced.length;i++) {
-        await formatSendSubmission(channel, unannounced[i]);
+        for (let i = 0;i<unannounced.length;i++) {
+            await formatSendSubmission(channel, unannounced[i]);
+        }
+        console.log(`Announced ${unannounced.length} submissions!`);
+    } catch(e) {
+        console.error(e);
+    } finally {
+        discordClient.destroy();
     }
-    console.log(`Announced ${unannounced.length} submissions!`);
-    discordClient.destroy();
 }
 
 main();
