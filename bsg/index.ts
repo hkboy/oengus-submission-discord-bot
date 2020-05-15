b4818078540932eaeb84cfafd92ebabcb7b76440
Riekelt <riekelt@hotmail.com>
2020-05-15 18:02:46 +0200

    Used a better ISO8601 -> Dur converter Shoutouts to Grumpmeister for the recommendation: https://jsfiddle.net/kbeb0p99/4/

--- bsg/index.ts
+++ bsg/index.ts
@@ -113,46 +113,65 @@ function changeTime(info) {
   getEventName();
   console.log(info.estimate);
   var timeNow = "";
-  heleMinuten = false;
-  geenUur = false;
-  enkeleDigit = false;
   timeNow = info.estimate;
-  if (timeNow.charAt(timeNow.length - 1) == "M") {
-    if ((!isNaN(timeNow.charAt(timeNow.length - 2))) && (isNaN(timeNow.charAt(timeNow.length - 3)))) {
-      enkeleDigit = true;
-    }
-  } else {
-    heleMinuten = true;
-  }
-  if (timeNow.charAt(3) != "H") {
-    geenUur = true;
-  }
-  timeNow = timeNow.substring(2).slice(0, -1);
-  timeNow = timeNow.replace("H", ":");
-  if (heleMinuten) {
-    timeNow += ":00";
-  }
-  if (geenUur) {
-    timeNow = "00:" + timeNow;
-  } else {
-    timeNow = "0" + timeNow;
-  }
-  console.log(timeNow + " OG");
-  if (enkeleDigit) {
-    timeNow = (insert(timeNow, "0", -1));
-  }
+  timeNow = parseDuration(timeNow);
   return timeNow;
 }
 
-function insert(main_string, ins_string, pos) {
-  if (typeof (pos) == "undefined") {
-    pos = 0;
+function parseDuration(PT) {
+  var output = [];
+  var durationInSec = 0;
+  var matches = PT.match(/P(?:(\d*)Y)?(?:(\d*)M)?(?:(\d*)W)?(?:(\d*)D)?T?(?:(\d*)H)?(?:(\d*)M)?(?:(\d*)S)?/i);
+  var parts = [
+    { // years
+      pos: 1,
+      multiplier: 86400 * 365
+    },
+    { // months
+      pos: 2,
+      multiplier: 86400 * 30
+    },
+    { // weeks
+      pos: 3,
+      multiplier: 604800
+    },
+    { // days
+      pos: 4,
+      multiplier: 86400
+    },
+    { // hours
+      pos: 5,
+      multiplier: 3600
+    },
+    { // minutes
+      pos: 6,
+      multiplier: 60
+    },
+    { // seconds
+      pos: 7,
+      multiplier: 1
+    }
+  ];
+
+  for (var i = 0; i < parts.length; i++) {
+    if (typeof matches[parts[i].pos] != 'undefined') {
+      durationInSec += parseInt(matches[parts[i].pos]) * parts[i].multiplier;
+    }
   }
-  if (typeof (ins_string) == "undefined") {
-    ins_string = '';
+
+  // Hours extraction
+  if (durationInSec > 3599) {
+    output.push(parseInt(durationInSec / 3600));
+    durationInSec %= 3600;
   }
-  return main_string.slice(0, pos) + ins_string + main_string.slice(pos);
-}
+  // Minutes extraction with leading zero
+  output.push(('0' + parseInt(durationInSec / 60)).slice(-2));
+  // Seconds extraction with leading zero
+  output.push(('0' + durationInSec % 60).slice(-2));
+
+  return output.join(':');
+};
+
 
 async function getAlreadyAnnouncedCategoryIDs(channel: Discord.TextChannel): Promise<number[]> {
   let categoryIDs: number[] = [];
